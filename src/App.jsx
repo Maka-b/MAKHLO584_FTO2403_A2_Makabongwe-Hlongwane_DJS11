33eb869918a600694560b9f547170cfbfb696765
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-08-17 12:58:36 +0200

    tailwind configured

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="text-3xl font-bold underline text-blue-600">
+      Hello world!
+    </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
