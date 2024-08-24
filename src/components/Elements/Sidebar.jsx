90cbdf22fd7873a9b0d6bc919b7c93aebbcb88c8
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-08-24 11:31:47 +0200

    sidebar close fixed

--- src/components/Elements/Sidebar.jsx
+++ src/components/Elements/Sidebar.jsx
@@ -5,6 +5,7 @@ import { RiCloseLine } from "react-icons/ri"
 import { logo } from "../assets"
 import {links} from "../assets/constants"
 import { HiDesktopComputer, HiOutlineAcademicCap, HiOutlineMenu } from "react-icons/hi"
+import TestIcon from "./TestIcon"
 
 const NavLinks =( {handleClick} ) => (
     <div className="mt-10">
@@ -28,20 +29,25 @@ export default function Sidebar(){
         <>
             <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
                 <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+                
                 <NavLinks/>
             </div>
             
             <div className="absolute md:hidden block top-6">
-                {mobileMenuOpen ? 
-                    <RiCloseLine className="w-5 text-green mr-2"
-                    onClick={()=> setMobileMenuOpen(true)} />
-                    : <HiOutlineMenu
+                {mobileMenuOpen ? (
+                    <RiCloseLine className="w-5 text-black mr-2"
+                    onClick={()=> setMobileMenuOpen(false)} />
+                    
+                ): <HiOutlineMenu
                         onClick={()=> setMobileMenuOpen(true)}                    
-                        className="w-5 text-green mr-2"/>
+                        className="w-5 text-green-800 mr-2"/>
                 }
             </div>
-            <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-t1 from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${ mobileMenuOpen ? 'left-0':'left-full'}`}> 
+            <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-t1 from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden ${ mobileMenuOpen ? 'left-0':'-left-full'}`}> 
                 <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+                <RiCloseLine className="w-5 text-black mr-2"
+                    onClick={()=> setMobileMenuOpen(false)} />
+
                 <NavLinks handleClick={()=>{setMobileMenuOpen(false)}} />
             </div>
         </>
---
+++ src/components/Elements/TestIcon.jsx
@@ -0,0 +1,9 @@
+import { RiCloseLine } from "react-icons/ri";
+
+export default function TestIcon() {
+    return (
+        <div>
+            <RiCloseLine className="w-8 h-8 text-black" />
+        </div>
+    );
+}
